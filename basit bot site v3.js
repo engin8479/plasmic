f4cc9454076770da9e3644fb67824d6f10707a00
engin8479 <74909017+engin8479@users.noreply.github.com>
2021-03-21 12:57:39 +0300

    Create basit bot site v3.js

---
+++ basit bot site v3.js
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<head>
+<body style="background-color: mediumturquoise;">
+    <a href="#"><img src="resim.png" alt="Örnek Resim" border="0"/></a>
+    <script type="text/javascript" src="https://webkodu.ozgurlukicin.com/kod-kaynak/wk-kar-efekt.js"></script>
+    <title>Enginar</title>
+    <br>
+</center><b style="color: darkred;">Plasmic code boş site kodu! Güle güle kullanın :D</b></center>
+
+
+<br></br>
+<input style="color: maroon;" style="background-color: mediumseagreen;" type="submit" onClick="parent.location='https://discord.gg/javascript'" value='Discord sunucumuz'>
+<input type="button" onClick="location.href='site.html'" value="Siteye geçmek için tıkla!" />
+
+</body>
+
+</head>
+//
+<!DOCTYPE html>
+<head>
+    <script type="text/javascript" src="https://webkodu.ozgurlukicin.com/kod-kaynak/wk-kar-efekt.js"></script>
+<body style="background-color: rgb(0, 0, 0);">
+<center> <a href="#"><img src="lazım.gif" alt="" border="0"/></a> </center>
+<article>
+    <center><button style="background-color: teal;"  style="color: sienna;" onclick="window.location.href = 'https://discordapp.com/oauth2/authorize?client_id=733720326646792263&scope=bot&permissions=2146958847';">Davet etmek için tıkla!</button></center>
+   <br>
+   <center><button style="background-color: teal;"  style="color: sienna;" onclick="window.location.href = 'https://discord.gg/javascript';">Destek sunucusu için tıkla!</button></center>
+</article>
+
+
+           <center> <table border="10" style="color: cornflowerblue;">
+                <tr>
+                    <td style="color: rgb(7, 253, 68);">Komut Katagorileri</td>
+                    <td style="color: rgb(7, 253, 68);">Moderasyon</td>
+                    <td style="color: rgb(7, 253, 68);">Eğlence</td>
+                    <td style="color: rgb(7, 253, 68);">Emoji</td>
+                    <td style="color: rgb(7, 253, 68);">Koruma<br />
+                    </td>
+                </tr>
+                <tr>
+                    <td style="color: rgb(7, 253, 68);">Komut sayısı:</td>
+                    <td style="color: rgb(7, 253, 68);">10</td>
+                    <td style="color: rgb(7, 253, 68);">8</td>
+                    <td style="color: rgb(7, 253, 68);">5</td>
+                    <td style="color: rgb(7, 253, 68);">15</td>
+                </tr>
+                <tr>
+                    <br>
+                    <br>
+                    <i style="color: cyan;">YARDIM MENÜLERİ</i>
+                   <br>
+                    <b style="color: seagreen;">n!yardım = Ana komutları gösterir.</b>
+                    <br>
+                    <b style="color: seagreen;">n!moderasyon = Moderasyon komutlarını gösterir.</b>
+                    <br>
+                    <b style="color: seagreen;">n!eğlence = Eğlence komutlarını gösterir.</b>
+                    <br>
+                    <b style="color: seagreen;">n!emoji = Emoji komutlarını gösterir.</b>
+                    <br>
+                    <b style="color: seagreen;">n!antiraid = Güvenlik komutlarını gösterir.</b>
+<br> <br>
+<br>
+<br>
+<i style="color: cyan;">KOMUTLARI</i>
+<br>
+<b style="color: seagreen;">n!komut = Komut özellik</b>
+<br>
+<b style="color: seagreen;">n!komut = Komut özellik</b>
+<br>
+<b style="color: seagreen;">n!komut = Komut özellik</b>
+<br>
+<b style="color: seagreen;">n!komut = Komut özellik</b>
+<br>
+<b style="color: seagreen;">n!komut = Komut özellik</b>
+<br>
+<br> <br>
+<i style="color: cyan;">KOMUT TABLOSU</i>
+</tr>
+
+</body>
+</head>
